66ae4a5ffa3572d3e23e011486793f0d19ad9d12
ngochdm <ngochdm@gmail.com>
2025-12-29 16:34:11 +0900

    update validation_ablation

--- baselines/validation_ablation.sh
+++ baselines/validation_ablation.sh
@@ -1,14 +1,12 @@
 # usage: bash filename.sh
 
 DATA_PATH='NGOCHDM/Dataset/'
-DATA_NAME='SUWON_DATASET_VER02-01'
+DATA_NAME='TSBOW'
 
 MODEL_PATH='yolov12/runs/detect/'
-# MODEL_NAME='yolo11x'
 
 CUDA_DEVICE='cuda:1'
-IMAGE_SIZE=1280     #640 #1280
-# BATCH_SIZE=128
+IMAGE_SIZE=1280 
 CONF_SCORE=0.5
 IOU_DETECT=0.6
 SPLIT_PATH='test'
@@ -21,17 +19,6 @@ BATCH_SIZE_RTDETRl=14
 
 # remove cache file
 # rm "${DATA_PATH}${DATA_NAME}/test/labels.cache"
-
-# python validation_YOLO.py \
-#     -d $DATA_PATH   -n $DATA_NAME \
-#     -p $MODEL_PATH  -m "EVAL/yolov8x_25epochs" \
-#     -c $CUDA_DEVICE -i $IMAGE_SIZE  -b $BATCH_SIZE_YOLOv8x  --conf $CONF_SCORE  --iou $IOU_DETECT    --split $SPLIT_PATH    --max_det $MAX_DETECT
-
-# python validation_YOLO.py \
-#     -d $DATA_PATH   -n $DATA_NAME \
-#     -p $MODEL_PATH  -m "ABLATION/yolov12x_SUWON_DATASET_VER02-01" \
-#     -c $CUDA_DEVICE -i $IMAGE_SIZE  -b $BATCH_SIZE_YOLO11x \
-#     --conf $CONF_SCORE  --iou $IOU_DETECT    --split $SPLIT_PATH    --max_det $MAX_DETECT
 
 ##############################
 
@@ -52,10 +39,10 @@ CUDA_DEVICE='cuda:2'
 for scenario in $class_sn_value
 do
     python validation_YOLO.py \
-        -d $DATA_PATH   -n $DATA_NAME   -y "${VERSION_NAME}_SCENARIO_${scenario}" \
-        -p $MODEL_PATH  -m "ABLATION/yolov12x_SUWON_DATASET_VER02-01" \
-        -c $CUDA_DEVICE -i $IMAGE_SIZE  -b $BATCH_SIZE_YOLO12x   \
-        --conf $CONF_SCORE      --iou $IOU_DETECT    --split $SPLIT_PATH    \
+        -d $DATA_PATH   -n $DATA_NAME   -y "${VERSION_NAME}_SCENARIO_${scenario}"   \
+        -p $MODEL_PATH  -m "ABLATION/yolov12x_TSBOW"                                \
+        -c $CUDA_DEVICE -i $IMAGE_SIZE  -b $BATCH_SIZE_YOLO12x                      \
+        --conf $CONF_SCORE      --iou $IOU_DETECT    --split $SPLIT_PATH            \
         --max_det $MAX_DETECT   --task "ablation"
 done
 
@@ -66,10 +53,10 @@ CUDA_DEVICE='cuda:1'
 for weather in $class_w_value
 do
     python validation_YOLO.py \
-        -d $DATA_PATH   -n $DATA_NAME   -y "${VERSION_NAME}_WEATHER_${weather}" \
-        -p $MODEL_PATH  -m "ABLATION/yolov12x_SUWON_DATASET_VER02-01" \
-        -c $CUDA_DEVICE -i $IMAGE_SIZE  -b $BATCH_SIZE_YOLO12x   \
-        --conf $CONF_SCORE  --iou $IOU_DETECT    --split $SPLIT_PATH    \
+        -d $DATA_PATH   -n $DATA_NAME   -y "${VERSION_NAME}_WEATHER_${weather}"     \
+        -p $MODEL_PATH  -m "ABLATION/yolov12x_TSBOW"                                \
+        -c $CUDA_DEVICE -i $IMAGE_SIZE  -b $BATCH_SIZE_YOLO12x                      \
+        --conf $CONF_SCORE  --iou $IOU_DETECT    --split $SPLIT_PATH                \
         --max_det $MAX_DETECT   --task "ablation"
 done
 
@@ -80,10 +67,10 @@ CUDA_DEVICE='cuda:2'
 for scale in $class_s_value
 do
     python validation_YOLO.py \
-        -d $DATA_PATH   -n $DATA_NAME   -y "${VERSION_NAME}_SCALE_${scale}" \
-        -p $MODEL_PATH  -m "ABLATION/yolov12x_SUWON_DATASET_VER02-01" \
-        -c $CUDA_DEVICE -i $IMAGE_SIZE  -b $BATCH_SIZE_YOLO12x   \
-        --conf $CONF_SCORE  --iou $IOU_DETECT    --split $SPLIT_PATH    \
+        -d $DATA_PATH   -n $DATA_NAME   -y "${VERSION_NAME}_SCALE_${scale}"         \
+        -p $MODEL_PATH  -m "ABLATION/yolov12x_TSBOW"                                \
+        -c $CUDA_DEVICE -i $IMAGE_SIZE  -b $BATCH_SIZE_YOLO12x                      \
+        --conf $CONF_SCORE  --iou $IOU_DETECT    --split $SPLIT_PATH                \
         --max_det $MAX_DETECT   --task "ablation"
 done
 
@@ -94,10 +81,10 @@ CUDA_DEVICE='cuda:3'
 for roadtype in $class_r_value
 do
     python validation_YOLO.py \
-        -d $DATA_PATH   -n $DATA_NAME   -y "${VERSION_NAME}_ROADTYPE_${roadtype}" \
-        -p $MODEL_PATH  -m "ABLATION/yolov12x_SUWON_DATASET_VER02-01" \
-        -c $CUDA_DEVICE -i $IMAGE_SIZE  -b $BATCH_SIZE_YOLO12x   \
-        --conf $CONF_SCORE  --iou $IOU_DETECT    --split $SPLIT_PATH    \
+        -d $DATA_PATH   -n $DATA_NAME   -y "${VERSION_NAME}_ROADTYPE_${roadtype}"   \
+        -p $MODEL_PATH  -m "ABLATION/yolov12x_TSBOW"                                \
+        -c $CUDA_DEVICE -i $IMAGE_SIZE  -b $BATCH_SIZE_YOLO12x                      \
+        --conf $CONF_SCORE  --iou $IOU_DETECT    --split $SPLIT_PATH                \
         --max_det $MAX_DETECT   --task "ablation"
 done
 
@@ -108,9 +95,9 @@ CUDA_DEVICE='cuda:1'
 for traffic in $class_t_value
 do
     python validation_YOLO.py \
-        -d $DATA_PATH   -n $DATA_NAME   -y "${VERSION_NAME}_TRAFFIC_${traffic}" \
-        -p $MODEL_PATH  -m "ABLATION/yolov12x_SUWON_DATASET_VER02-01" \
-        -c $CUDA_DEVICE -i $IMAGE_SIZE  -b $BATCH_SIZE_YOLO12x   \
-        --conf $CONF_SCORE  --iou $IOU_DETECT    --split $SPLIT_PATH    \
+        -d $DATA_PATH   -n $DATA_NAME   -y "${VERSION_NAME}_TRAFFIC_${traffic}"     \
+        -p $MODEL_PATH  -m "ABLATION/yolov12x_TSBOW"                                \
+        -c $CUDA_DEVICE -i $IMAGE_SIZE  -b $BATCH_SIZE_YOLO12x                      \
+        --conf $CONF_SCORE  --iou $IOU_DETECT    --split $SPLIT_PATH                \
         --max_det $MAX_DETECT   --task "ablation"
 done
