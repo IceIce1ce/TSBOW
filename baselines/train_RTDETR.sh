c310995347800b110a4ee5402c1c626d33e23bc4
ngochdm <ngochdm@gmail.com>
2025-12-30 15:19:56 +0900

    update sh files

--- baselines/train_RTDETR.sh
+++ baselines/train_RTDETR.sh
@@ -2,24 +2,19 @@
 DATA_PATH='NGOCHDM/Dataset/'
 DATA_NAME='TSBOW'
 
-# Model_ver can be: yolov8, yolo11, yolov12
-# Weights can be: n, s, m, l, x
-MODEL_VER='yolov12x'
+# Choose model version: 'rtdetr-l', 'rtdetr-x'
+MODEL_VER='rtdetr-x'
 
 CUDA_DEVICE='[0,1,2,3]'
 EPOCH_TRAIN=100
-BATCH_SIZE=40
+BATCH_SIZE=36
 IMAGE_SIZE=1280
-CACHE=True
-
-TRAIN_STATE='start'
 
 export NCCL_IB_GID_INDEX=3
 export NCCL_P2P_DISABLE=1
 
-python train_YOLO.py \
+python train_RTDETR.py \
     -d $DATA_PATH   -n $DATA_NAME \
     -m $MODEL_VER   -c $CUDA_DEVICE \
     -e $EPOCH_TRAIN -b $BATCH_SIZE \
-    -i $IMAGE_SIZE  --cache $CACHE \
-    --state $TRAIN_STATE
+    -i $IMAGE_SIZE
