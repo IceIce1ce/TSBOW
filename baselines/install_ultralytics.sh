43eae4022ee537c3b20b909615c4f1c60f027286
ngochdm <ngochdm@gmail.com>
2026-02-10 17:16:51 +0900

    add install_ultralytics for baselines

---
+++ baselines/install_ultralytics.sh
@@ -0,0 +1,16 @@
+
+echo "Create & activate ultralytics environment before installation"
+
+echo "conda create -n ultralytics  python=3.10 -y"
+
+pip install pytorch==2.2.2 torchvision==0.17.2 torchaudio==2.2.2 pytorch-cuda=11.8 -c pytorch -c nvidia
+
+git clone https://github.com/ultralytics/ultralytics 
+
+cd ultralytics 
+
+pip install -e .
+
+pip install tensorboard
+
+echo "DONE: ultralytics is installed"
