d87e082eede9c8c6d2b36862c4469b3b136ed89b
ngochdm <ngochdm@gmail.com>
2026-03-12 18:11:07 +0900

    update version of XAnylabeling (suitable version for current document)

--- baselines/install_Xanylabeling.sh
+++ baselines/install_Xanylabeling.sh
@@ -9,7 +9,7 @@ echo "     activate: conda activate Xanylabeling"
 pip install onnxruntime-gpu --extra-index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/onnxruntime-cuda-12/pypi/simple/ 
 
 # Download the source code
-git clone https://github.com/CVHub520/X-AnyLabeling.git
+git clone -b v3.3.8 https://github.com/CVHub520/X-AnyLabeling.git
 cd X-AnyLabeling/
 
 # Install Dependencies
