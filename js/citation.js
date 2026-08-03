ea5963aed82fdabeaf04ef95f02776e82d1769bf
ngochdm <ngochdm@gmail.com>
2026-08-03 16:02:42 +0900

    recover main.js

--- js/citation.js
+++
@@ -1,8 +0,0 @@
-export function copyBibtex() {
-    const code = document.querySelector("#citation pre code").innerText;
-    navigator.clipboard.writeText(code);
-
-    const button = document.querySelector(".copy-btn");
-    button.innerText = "✅ Copied!";
-    setTimeout(() => button.innerText = "📋 Copy", 2000);
-}
